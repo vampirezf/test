a3235068b679f51553b359e9df24a9618ed94eca
zuofan <815479015@qq.com>
2021-07-27 08:28:48 +0800

    测试类添加

---
+++ test/src/main/java/dataStructureAndAlgorithm/Practice.java
@@ -0,0 +1,15 @@
+package dataStructureAndAlgorithm;
+
+import org.junit.Test;
+
+/**
+ * @author zf
+ * @date 2021/7/21 16:45
+ */
+public class Practice {
+    @Test
+    public void test1(){
+
+    }
+
+}
